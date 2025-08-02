807e2544240d99cff80abbf15a470ce715d4c045
Ameya Basrur <ameyabasrur@gmail.com>
2025-08-02 16:01:54 +0530

    v1 of fixed buggy script
    Version1 of fixed buggy script. Will try to refactor code wherever possible.

---
+++ news_summarizer_buggy.py
@@ -0,0 +1,114 @@
+import requests
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+
+BASE_URL = "https://newsapi.org/v1/articles"
+#Since newsapi v1 returns null for time published, we will have to use newsapi v2 to get date published.
+FILTER_URL = "https://newsapi.org/v2/everything"
+
+
+def get_articles():
+    url = f"{BASE_URL}?source=bbc-news&apiKey={os.getenv("API_KEY")}"
+    response = requests.get(url)
+    try:
+        #response.raise_for_status() to create an HTTPError object if the response status code is not in 200-299 range. 
+        response.raise_for_status()
+        articles = response.json()['articles']
+        return articles
+    except requests.exceptions.HTTPError as req_e:
+        print("Failed to fetch news\n")
+        print(f"HTTP error: {req_e.response.status_code}")
+        return []
+
+def get_keyword(filter_arr):
+    user_keyword_choice = input("Would you like to filter articles by keyword? (type yes to filter by keyword else type no to not filter by keyword)\n")
+    if user_keyword_choice.lower() != "yes" and user_keyword_choice.lower() != "no":
+        while True:
+            user_keyword_choice = input("Error. Wrong input. Please enter yes or no only\n")
+            if user_keyword_choice.lower() == "yes" or user_keyword_choice.lower() == "no":
+                break
+    if user_keyword_choice.lower() == "yes":
+        keyword_str = input("Enter the keyword you would like the articles presented to include:\n")
+        filtered_keyword_str = "q=" + keyword_str
+        filter_arr.append(filtered_keyword_str)
+        print("Keywords included. Moving to date range finder.")
+    else:
+        print("Keywords inclusion skipped. Moving to date range finder.")
+
+def get_date_range(filter_arr):
+    user_date_range_choice = input("Would you like to filter articles by include a from and to date? (type yes to filter by date range else type no to not filter by date)\n")
+    if user_date_range_choice.lower() != "yes" and user_date_range_choice.lower() != "no":
+        while True:
+            user_date_range_choice = input("Error. Wrong input. Please enter yes or no only\n")
+            if user_date_range_choice.lower() == "yes" or user_date_range_choice.lower() == "no":
+                break
+    if user_date_range_choice.lower() == "yes":
+        while True:
+            from_date_str = input("Enter the start date to search from in (YYYY-MM-DD) format\n")
+            try:
+                from_date_datetime = datetime.strptime(from_date_str,"%Y-%m-%d")
+                print(f"From Date: {from_date_datetime.date()}")
+                break
+            except ValueError as date_ve:
+                print(f"Error: {date_ve}")
+                print("Invalid date. Try again by providing the correct date in the specified format")
+        while True:
+            to_date_str = input("Enter the end date of the search in (YYYY-MM-DD) format\n")
+            try:
+                to_date_datetime = datetime.strptime(to_date_str,"%Y-%m-%d")
+                print(f"To Date: {to_date_datetime.date()}")
+                break
+            except ValueError as date_ve:
+                print(f"Error: {date_ve}")
+                print("Invalid date. Try again by providing the correct date in the specified format")
+        from_date_filter = "from=" + str(from_date_datetime)
+        to_date_filter = "to=" + str(to_date_datetime)
+        filter_arr.append(from_date_filter)
+        filter_arr.append(to_date_filter)
+
+def get_filtered_articles(filter_arr):
+    filter_string = "&".join(filter_arr)
+    url = f"{FILTER_URL}?{filter_string}&sources=bbc-news&apiKey={os.getenv("API_KEY")}"
+    response = requests.get(url)
+    try:
+        #response.raise_for_status() to create an HTTPError object if the response status code is not in 200-299 range. 
+        response.raise_for_status()
+        articles = response.json()['articles']
+        return articles
+    except requests.exceptions.HTTPError as req_e:
+        print("Failed to fetch news\n")
+        print(f"HTTP error: {req_e.response.status_code}")
+        return []
+    
+
+def summarize(articles):
+    for art in articles:
+        print("Title:", art['title'])
+        print("Description:", art['description'])
+        print("URL:", art['url'])
+        print("----")
+
+
+def main():
+    try:
+        load_dotenv()
+        filters = []
+        get_keyword(filters)
+        get_date_range(filters)
+        #check if the user has specific filters for articles. If no then execute the original function.
+        if filters:
+            filtered_articles = get_filtered_articles(filters)
+            if filtered_articles:
+                summarize(filtered_articles)
+        else:
+            articles = get_articles()
+            if articles:
+                summarize(articles)
+    except Exception as art_e:
+        print(f"\nSomething went wrong: {type(art_e)}")
+        print(art_e)
+
+
+main()
