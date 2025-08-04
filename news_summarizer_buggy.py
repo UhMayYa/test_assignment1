af788aac4f84928735d4cf20c43de64a9212e4b3
Ameya Basrur <ameyabasrur@gmail.com>
2025-08-04 18:31:37 +0530

    fixed buggy script v2
    Added proper logging to error messages and refactored redundant code

--- news_summarizer_buggy.py
+++ news_summarizer_buggy.py
@@ -2,15 +2,23 @@ import requests
 from datetime import datetime
 import os
 from dotenv import load_dotenv
+import logging
 
 
 BASE_URL = "https://newsapi.org/v1/articles"
 #Since newsapi v1 returns null for time published, we will have to use newsapi v2 to get date published.
 FILTER_URL = "https://newsapi.org/v2/everything"
 
+#Format the log level and what the log will show in console
+logging.basicConfig(level=logging.ERROR, format='%(levelname)s : %(message)s')
 
-def get_articles():
-    url = f"{BASE_URL}?source=bbc-news&apiKey={os.getenv("API_KEY")}"
+
+def get_articles(filters):
+    if filters:
+        filter_string = "&".join(filters)
+        url = f"{FILTER_URL}?{filter_string}&sources=bbc-news&apiKey={os.getenv("API_KEY")}"
+    else:
+        url = f"{BASE_URL}?source=bbc-news&apiKey={os.getenv("API_KEY")}"
     response = requests.get(url)
     try:
         #response.raise_for_status() to create an HTTPError object if the response status code is not in 200-299 range. 
@@ -18,8 +26,9 @@ def get_articles():
         articles = response.json()['articles']
         return articles
     except requests.exceptions.HTTPError as req_e:
-        print("Failed to fetch news\n")
-        print(f"HTTP error: {req_e.response.status_code}")
+        #print("Failed to fetch news\n")
+        logging.exception(f"HTTP error: {req_e.response.status_code}")
+        #print(f"HTTP error: {req_e.response.status_code}")
         return []
 
 def get_keyword(filter_arr):
@@ -59,6 +68,15 @@ def get_date_range(filter_arr):
             try:
                 to_date_datetime = datetime.strptime(to_date_str,"%Y-%m-%d")
                 print(f"To Date: {to_date_datetime.date()}")
+                if to_date_datetime < from_date_datetime:
+                    while to_date_datetime < from_date_datetime:
+                        to_date_str = input("Article search end date cannot be before the Article search start date. Please enter a vaild end date in (YYYY-MM-DD) format.\n")
+                        try:
+                            to_date_datetime = datetime.strptime(to_date_str,"%Y-%m-%d")
+                            print(f"New To Date: {to_date_datetime.date()}")
+                        except ValueError as new_ve:
+                            print(f"Error: {new_ve}")
+                            print("Invalid date. Try again by providing the correct date in the specified format")
                 break
             except ValueError as date_ve:
                 print(f"Error: {date_ve}")
@@ -68,19 +86,20 @@ def get_date_range(filter_arr):
         filter_arr.append(from_date_filter)
         filter_arr.append(to_date_filter)
 
-def get_filtered_articles(filter_arr):
-    filter_string = "&".join(filter_arr)
-    url = f"{FILTER_URL}?{filter_string}&sources=bbc-news&apiKey={os.getenv("API_KEY")}"
-    response = requests.get(url)
-    try:
-        #response.raise_for_status() to create an HTTPError object if the response status code is not in 200-299 range. 
-        response.raise_for_status()
-        articles = response.json()['articles']
-        return articles
-    except requests.exceptions.HTTPError as req_e:
-        print("Failed to fetch news\n")
-        print(f"HTTP error: {req_e.response.status_code}")
-        return []
+# def get_filtered_articles(filter_arr):
+#     filter_string = "&".join(filter_arr)
+#     url = f"{FILTER_URL}?{filter_string}&sources=bbc-news&apiKey={os.getenv("API_KEY")}"
+#     response = requests.get(url)
+#     try:
+#         #response.raise_for_status() to create an HTTPError object if the response status code is not in 200-299 range. 
+#         response.raise_for_status()
+#         articles = response.json()['articles']
+#         return articles
+#     except requests.exceptions.HTTPError as req_e:
+#         #print("Failed to fetch news\n")
+#         logging.exception(f"HTTP error: {req_e.response.status_code}")
+#         #print(f"HTTP error: {req_e.response.status_code}")
+#         return []
     
 
 def summarize(articles):
@@ -98,17 +117,18 @@ def main():
         get_keyword(filters)
         get_date_range(filters)
         #check if the user has specific filters for articles. If no then execute the original function.
-        if filters:
-            filtered_articles = get_filtered_articles(filters)
-            if filtered_articles:
-                summarize(filtered_articles)
-        else:
-            articles = get_articles()
-            if articles:
-                summarize(articles)
-    except Exception as art_e:
-        print(f"\nSomething went wrong: {type(art_e)}")
-        print(art_e)
+        # if filters:
+        #     filtered_articles = get_filtered_articles(filters)
+        #     if filtered_articles:
+        #         summarize(filtered_articles)
+        # else:
+        articles = get_articles(filters)
+        if articles:
+            summarize(articles)
+    except Exception:
+        logging.exception("Something went wrong")
+        # print(f"\nSomething went wrong: {type(art_e)}")
+        # print(art_e)
 
 
 main()
